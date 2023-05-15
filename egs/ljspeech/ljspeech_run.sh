dc63c464299910990325222cf1f059a50c421c82
ceastld <1950946176@qq.com>
2023-05-15 20:10:39 +0800

    demos

---
+++ egs/ljspeech/ljspeech_run.sh
@@ -0,0 +1,10 @@
+exp_dir=exp/valle1
+
+CUDA_VISIBLE_DEVICES=0 python bin/infer.py --output-dir infer/demos \
+    --model-name valle --norm-first true --add-prenet false \
+    --share-embedding true --norm-first true --add-prenet false \
+    --text-prompts "" \
+    --audio-prompts "" \
+    --text ljspeech.txt \
+    --output-dir "infer/demos" \
+    --checkpoint ${exp_dir}/best-valid-loss.pt
